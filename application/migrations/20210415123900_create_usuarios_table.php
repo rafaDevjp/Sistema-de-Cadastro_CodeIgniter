f4381d7e22b6c003453c34ad01c098201604c9e8
Rafael Sulpino de Andrade <rafa.devjp@gmail.com>
2021-04-15 12:58:13 -0300

    adicão de reamificaçoes

---
+++ application/migrations/20210415123900_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_blog extends CI_Migration {
+
+        public function up()
+        {
+                $this->dbforge->add_field(array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'constraint' => 5,
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                        ),
+                        'nome' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '100',
+                        ),
+                        'email' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '255',
+                        ),
+                        'senha' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '50',
+                        )
+                ));
+
+                $this->dbforge->add_key('id', TRUE);
+                $this->dbforge->create_table('usuarios_table');
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('usuarios_table');
+        }
+}
