570d858f48bda8bd4faa58c5cb268adb6f4cf8e7
Rafael <rafael.andradejppb@gmail.com>
2021-04-18 17:26:39 -0300

    adição da imagem da tela home

---
+++ application/views/menssage/delete_success.php
@@ -0,0 +1,8 @@
+<div class="row justify-content-end p-2 position-absolute col-lg-12">
+    <div class="alert alert-success alert-dismissible fade show col-3 p-3 text-center" role="alert">
+              <strong>Deletado! </strong> usuário deletado com sucesso !
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+    </div>
+</div>
---
+++ application/views/menssage/update_success.php
@@ -0,0 +1,8 @@
+<div class="row justify-content-end p-2 position-absolute col-lg-12">
+    <div class="alert alert-success alert-dismissible fade show col-3 p-3 " role="alert">
+              <strong>Atualizado! </strong> dados atualizados com sucesso !
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+    </div>
+</div>
